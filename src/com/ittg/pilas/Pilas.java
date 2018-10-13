1362f4c007bb41a065d7c999acce16830be69540
Yuliani <yuliani_m@hotmail.com>
2018-10-12 21:28:25 -0500

    Creando metodos, agregarElemento y eliminar

--- src/com/ittg/pilas/Pilas.java
+++ src/com/ittg/pilas/Pilas.java
@@ -1,29 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ittg.pilas;
-import java.util.*;
+
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author DellLatitude1
- */
 public class Pilas {
-    
-    
-    public void tamaño(){
-      int  a[]=new int[5];
-      for(int i=0; i<a.length; i++){
-       a[i]=Integer.parseInt(JOptionPane.showInputDialog(null, "Escribe un numero"));
-        }
-      System.out.println(a.length);
+
+    private int tope, max;
+    private int pila[];
+
+    public Pilas() {
+
     }
-    
-    public static void main(String [] args){
-    Pilas x=new Pilas();
-    x.tamaño();
+
+    public Pilas(int m) {
+        max = m;
+        pila = new int[max];
+        tope = 0;
+        JOptionPane.showMessageDialog(null, "Se creo pila de: "
+                + max + " elementos");
+    }
+
+    private void agregarElemento(int n) {
+        if (this.tope < this.max) {
+            pila[tope] = n;
+            tope++;
+            JOptionPane.showMessageDialog(null, "Dato agregado correctamente");
+        } else {
+            JOptionPane.showMessageDialog(null, "No hay espacio disponible");
+        }
+    }
+
+    private void menu() {
+        int opcion;
+        Pilas p = new Pilas(Integer.parseInt(
+                JOptionPane.showInputDialog(
+                        "Escribe el tamaño de la pila: ")));
+        do {//inicia el do
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("Menu:\n"
+                    + "1.- Insertar elemento\n"
+                    + "2.- Quitar elemento\n"
+                    + "3.- Imprimir pila\n"
+                    + "4.- Salir\n"));
+            switch (opcion) {
+                case 1:
+                    p.agregarElemento(Integer.parseInt(JOptionPane.showInputDialog("Ingresa elemento")));
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null,
+                            "Opcion invalida");
+            }
+        }//Finaliza el do
+        while (true);
+
+    }
+
+    public static void main(String[] args) {
+        Pilas p = new Pilas();
+        p.menu();
     }
 }
